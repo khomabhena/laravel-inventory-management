5a8830f98fa8bdd2d97ec7bbc823af141b047d5a
Kholwani Mabhena <colwanymab@gmail.com>
2024-04-10 17:08:49 +0200

    Seeding provider data

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Provider;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,9 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password'=> bcrypt('123456789'),
         ]);
+
+        Provider::factory(5)->create();
     }
 }
